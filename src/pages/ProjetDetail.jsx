85c59bb5ed629ec28f2b605d02f2f69a256f821d
jeromepinheiropro-spec <jeromepinheiro.pro@gmail.com>
2026-07-28 23:42:27 +0200

    ProjetDetail: index realisations flagged index:true (Cerberion), noindex others

--- src/pages/ProjetDetail.jsx
+++ src/pages/ProjetDetail.jsx
@@ -111,7 +111,7 @@ export default function ProjetDetail() {
             : `${pFr.client} Découvrez ce que notre groupe a réalisé : ${pFr.tags.toLowerCase()}.`
         }
         path={`/realisations/${pFr.slug}`}
-        noindex
+        noindex={!pFr.index}
         jsonLd={[
           {
             "@context": "https://schema.org",
